7b3da93e3416c9dfd90621a82caf0337d3214253
Alex Adia <alex.adia1996@gmail.com>
2018-12-05 13:37:05 -0500

    committing alex and kari working on state level

---
+++ state_alexkari.R
@@ -0,0 +1,20 @@
+library(rvest)
+library(httr)
+library(dplyr)
+library(jsonlite)
+library(XML)
+library(stringr)
+library(zipcode)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(stringi)
+library(roxygen2)
+library(testthat)
+
+#1. All zipcodes from the county
+#2. Uses zipcodes to pull NPI providers
+#3. Link the datasets through Jackie's join function
+#4. Simplify the NPI results
+#5. Return the summary measure
+
