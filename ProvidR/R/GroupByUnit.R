4e20bb803a7ffa2a071d66410124ce0e544833d7
Meghan Peterson <meghan_peterson@brown.edu>
2018-12-11 12:49:28 -0500

    committing

--- ProvidR/R/GroupByUnit.R
+++ ProvidR/R/GroupByUnit.R
@@ -5,36 +5,59 @@ GroupByUnit<-function(data, unit){
       group_by(zipcode) %>%
       count() %>%
       arrange(n)
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 8cbebc6441b8da355e39f4f449cddee018b55a05
     return(countbyzip)
   } else if (unit==="county") {
     countbycounty<-data%>%
       group_by(county) %>%
       count() %>%
       arrange(n)
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 8cbebc6441b8da355e39f4f449cddee018b55a05
     return(countbycounty)
   } else if (unit=="state"){
     countbystate<-data%>%
       group_by(state) %>%
       count() %>%
       arrange(n)
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 8cbebc6441b8da355e39f4f449cddee018b55a05
     return(countbystate)
   } else if (unit=="city"){
     countbycity<-data%>%
       group_by(city) %>%
       count() %>%
       arrange(n)
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 8cbebc6441b8da355e39f4f449cddee018b55a05
     return(countbycity)
   } else if (unit=="type"){
     countbyprovider<-data%>%
       group_by(Primary_Taxonomy) %>%
       count() %>%
       arrange(desc(n))
+<<<<<<< HEAD
     
     return(countbyprovider)
   }
 }
  
+=======
+
+    return(countbyprovider)
+  }
+}
+>>>>>>> 8cbebc6441b8da355e39f4f449cddee018b55a05
--- ProvidR/R/ZipsFromState.R
+++ ProvidR/R/ZipsFromState.R
@@ -1,3 +1,12 @@
+#`ZipsFromState
+#'
+#'This function, given a state, provides you with all of the zip codes in that state.
+#'@param state_name.
+#'@return a vector containing all of the zipcodes in a given state
+#'@example
+#'ZipsFromState("RI")
+#'ZipsFromState("CT")
+
 ZipsFromState<-function(state_name){
   zip_holder<-zipcode%>%
     filter(state==state_name)%>%
@@ -5,3 +14,4 @@ ZipsFromState<-function(state_name){
   zip_state<-zip_holder[,1]
   return(zip_state)
 }
+devtools::document("man")
