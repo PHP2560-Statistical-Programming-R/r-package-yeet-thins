703d25c69c7772a15ac0ee134888fadde542bbd3
jacquelinegoldman <43216207+jacquelinegoldman@users.noreply.github.com>
2018-12-11 13:21:04 -0500

    trying to build package

--- ProvidR/R/ZipsFromState.R
+++ ProvidR/R/ZipsFromState.R
@@ -3,7 +3,7 @@
 #' @param "State"
 #' @return Zipcodes from the given state'
 
-source("Libraries.R")
+source("CheckPackages.R")
 check_packages(c("rvest", "httr", "dplyr", "jsonlite", "XML", "stringr", "zipcode",
                  "ggplot2", "stringi", "roxygen2", "testthat"))
 
@@ -16,3 +16,5 @@ ZipsFromState<-function(state_name){
   zip_state<-zip_holder[,1]
   return(zip_state)
 }
+
+document()
