d27928b0ae285f706529151e3e0597bc650026bb
Kari Kusler <kari_kusler@brown.edu>
2018-12-13 15:08:22 -0500

    fixed select issue by unloading raster

--- ProvidR/R/DensityMapbyCounty.R
+++ ProvidR/R/DensityMapbyCounty.R
@@ -2,7 +2,7 @@
 #' Maps providers per 1000 people by county
 
 DensityMapByCounty<-function(state,taxonomy) {
-  df.sum <- SummaryByCounty(state,taxonomy)
+  dat.sum <- SummaryByCounty(state,taxonomy)
 
   #install/load required packages
   if(!require(raster)){
@@ -15,17 +15,19 @@ DensityMapByCounty<-function(state,taxonomy) {
     library(ggmap)
   }
 
-  state.name <- df.sum$STNAME
+  state.name <- dat.sum$STNAME
 
   us.shape<-getData('GADM', country='USA', level=2)  #Get the County (level 2) Shapefile for the US
   state.shape<-subset(us.shape,NAME_1==state.name[1])
-  df.sum$NAME_2 <- sub(pattern=" County", replacement="", df$CTYNAME)
+  dat.sum$NAME_2 <- sub(pattern=" County", replacement="", dat.sum$CTYNAME)
   state.map <- merge(state.shape, df.sum, by.x='NAME_2', by.y='NAME_2')
+  #unloading raster because it masks select
+  detach("package:raster", unload=TRUE)
   #making colors (white to red)
   p <- colorRampPalette(c("white", "red"))(128)
   palette(p)
   prov <- state.map$provider_density
   cols <- (prov - min(prov))/diff(range(prov))*127+1
   #the final product
-  plot(state.map, col=cols)
+  return(plot(state.map, col=cols))
 }
