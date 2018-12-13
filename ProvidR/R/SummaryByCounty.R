6810b9baf9da193839aea6034460da43a5f03a16
Kari Kusler <kari_kusler@brown.edu>
2018-12-13 15:39:38 -0500

    updated vignette to match functions

--- ProvidR/R/SummaryByCounty.R
+++ ProvidR/R/SummaryByCounty.R
@@ -1,7 +1,7 @@
 #'SummaryByCounty
-#'This function gives you the numver of providers in each county, the number of providers per 1,000 people, and the population of the county
+#'This function gives you the number of providers in each county, the number of providers per 1,000 people, and the population of the county
 #'@param a data from from provider in the state by county
-#'@return a data fram
+#'@return a data frame
 #
 
 SummaryByCounty<-function(state, taxonomy){
