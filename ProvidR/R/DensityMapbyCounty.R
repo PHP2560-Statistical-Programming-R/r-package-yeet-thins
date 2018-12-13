51e1b859a6ac92f4a43b60092cd25fe7fce3f688
Kari Kusler <kari_kusler@brown.edu>
2018-12-12 19:59:04 -0500

    fixed errors when adding DensityMapByCounty to R folder, updated manuel and vignette

--- ProvidR/R/DensityMapbyCounty.R
+++ ProvidR/R/DensityMapbyCounty.R
@@ -1,22 +1,22 @@
+#' DensityMapByCounty
+#' Maps providers per 1000 people by county
+
 DensityMapByCounty<-function(df,state) {
   #install/load required packages
   if(!require(raster)){
     install.packages("raster")
     library(raster)
   }
-  if(!require(dplyr){
+  if(!require(dplyr)) {
     install.packages("dplyr")
     library(dplyr)
   }
-  if(!require(ggmap){
+  if(!require(ggmap)) {
     install.packages("ggmap")
     library(ggmap)
   }
   us.shape<-getData('GADM', country='USA', level=2)  #Get the County (level 2) Shapefile for the US
   state.shape<-subset(us.shape,NAME_1==state)
-  
-  #test2 is county-level summary data from ProviderInStateByCounty
-  #get rid of "County" as that is not in the geospacial data
   df$NAME_2 <- sub(pattern=" County", replacement="", df$CTYNAME) 
   state.map <- merge(state.shape, test2, by.x='NAME_2', by.y='NAME_2')
   #making colors (white to red)
