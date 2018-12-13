ca0bfc04a3c56a5a03d42a9b047e2c41cbceab26
Kari Kusler <kari_kusler@brown.edu>
2018-12-12 19:48:41 -0500

    deleting unnecessary files and added map function to package

--- ProvidR/R/DensityMapbyCounty.R
+++ ProvidR/R/DensityMapbyCounty.R
@@ -1,40 +1,36 @@
-#this still needs to be functionified but it works
-#checking if packages are installed; installing them if not
-maps<-function(state,taxonomy){
+DensityMapByCounty<-function(df,state) {
   #install/load required packages
   if(!require(raster)){
     install.packages("raster")
     library(raster)
   }
-  
   if(!require(dplyr){
     install.packages("dplyr")
     library(dplyr)
   }
-  
   if(!require(ggmap){
     install.packages("ggmap")
     library(ggmap)
   }
+  us.shape<-getData('GADM', country='USA', level=2)  #Get the County (level 2) Shapefile for the US
+  state.shape<-subset(us.shape,NAME_1==state)
   
-  us<-getData('GADM', country='USA', level=2)  #Get the County Shapefile for the US
-  
-  unique(us$NAME_1)
-  
-  ri<-subset(us,NAME_1=="Rhode Island")
-
   #test2 is county-level summary data from ProviderInStateByCounty
   #get rid of "County" as that is not in the geospacial data
-  test2$NAME_2 <- sub(pattern=" County", replacement="", test2$CTYNAME) 
-  ri2 <- merge(ri, test2, by.x='NAME_2', by.y='NAME_2')
-
-  #making colors
+  df$NAME_2 <- sub(pattern=" County", replacement="", df$CTYNAME) 
+  state.map <- merge(state.shape, test2, by.x='NAME_2', by.y='NAME_2')
+  #making colors (white to red)
   p <- colorRampPalette(c("white", "red"))(128)
   palette(p)
-  prov <- ri2$provider_density
+  prov <- state.map$provider_density
   cols <- (prov - min(prov))/diff(range(prov))*127+1
   #the final product
-  plot(ri2, col=cols)
+  plot(state.map, col=cols)
 }
+
+
+
+
+
 
 
