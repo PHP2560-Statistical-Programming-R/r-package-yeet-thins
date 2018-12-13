8d16fd8b7975eca12c8f4e208801c16567434dba
Kari Kusler <kari_kusler@brown.edu>
2018-12-13 13:36:06 -0500

    integrating some of the packages

--- ProvidR/R/DensityMapbyCounty.R
+++ ProvidR/R/DensityMapbyCounty.R
@@ -1,24 +1,26 @@
 #' DensityMapByCounty
 #' Maps providers per 1000 people by county
 
-DensityMapByCounty<-function(df,state) {
+DensityMapByCounty<-function(state,taxonomy) {
+  df.sum <- SummaryByCounty(state,taxonomy)
+  
   #install/load required packages
   if(!require(raster)){
     install.packages("raster")
     library(raster)
   }
-  if(!require(dplyr)) {
-    install.packages("dplyr")
-    library(dplyr)
-  }
+  
   if(!require(ggmap)) {
     install.packages("ggmap")
     library(ggmap)
   }
+  
+  state.name <- df.sum$STNAME[1]
+  
   us.shape<-getData('GADM', country='USA', level=2)  #Get the County (level 2) Shapefile for the US
-  state.shape<-subset(us.shape,NAME_1==state)
-  df$NAME_2 <- sub(pattern=" County", replacement="", df$CTYNAME) 
-  state.map <- merge(state.shape, df, by.x='NAME_2', by.y='NAME_2')
+  state.shape<-subset(us.shape,NAME_1==state.name)
+  df.sum$NAME_2 <- sub(pattern=" County", replacement="", df$CTYNAME) 
+  state.map <- merge(state.shape, df.sum, by.x='NAME_2', by.y='NAME_2')
   #making colors (white to red)
   p <- colorRampPalette(c("white", "red"))(128)
   palette(p)
--- ProvidR/R/SummaryByCounty.R
+++ ProvidR/R/SummaryByCounty.R
@@ -4,18 +4,14 @@
 #'@return a data fram
 #
 
-SummaryByCounty<-function(df){
-  #install/load required packages
-  if(!require(dplyr)){
-    install.packages("dplyr")
-    library(dplyr)
-  }
+SummaryByCounty<-function(state, taxonomy){
+  df <- ProviderInStateByCounty(state,taxonomy)
   #process data
   rows<-df %>%
-    group_by(CTYNAME, POPESTIMATE2010, Abbreviation, state.name) %>%
+    group_by(CTYNAME, POPESTIMATE2010, state.name, STNAME) %>%
     count() %>%
-    filter(Abbreviation==state.name) %>%
-    select(CTYNAME, POPESTIMATE2010, state.name, n) %>%
+    filter(state.name==state) %>%
+    select(CTYNAME, POPESTIMATE2010, STNAME, n) %>%
     mutate(provider_density = n/POPESTIMATE2010*1000) %>%
     arrange(n)
   return(rows)
